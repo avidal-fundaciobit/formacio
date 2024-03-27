7583cb4fc318a137384285f296de84fb1ce203b3
Àngel Vidal <avidal@fundaciobit.org>
2024-03-27 14:10:12 +0100

    Crear programa simple java amb mvn archetype:generate #2

---
+++ formaciojavatest/src/main/java/org/fundaciobit/formacio/formaciojavatester/AppTester.java
@@ -0,0 +1,15 @@
+package org.fundaciobit.formacio.formaciojavatester;
+
+import org.fundaciobit.formacio.App;
+
+/**
+ * Hello world!
+ *
+ */
+public class AppTester 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Missatge: "+App.getMessage() );
+    }
+}
