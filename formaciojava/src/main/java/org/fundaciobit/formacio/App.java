13df64e4bf2179da7c916b597788a423a2282db7
Àngel Vidal <avidal@fundaciobit.org>
2024-03-27 14:10:54 +0100

    Crear programa simple java amb mvn archetype:generate #2

--- formaciojava/src/main/java/org/fundaciobit/formacio/App.java
+++ formaciojava/src/main/java/org/fundaciobit/formacio/App.java
@@ -5,7 +5,8 @@ package org.fundaciobit.formacio;
  *@author avidal
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+  
+    public static String getMessage () {
+        return "Hello World!";
     }
 }
