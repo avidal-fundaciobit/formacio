1a09217cdd08ed9268e1cc25486505849df8699c
Àngel Vidal <avidal@fundaciobit.org>
2024-04-26 11:15:37 +0200

    Formacio Java Finalitzat

---
+++ formaciojava/src/main/java/org/fundaciobit/formacio/App.java
@@ -0,0 +1,12 @@
+package org.fundaciobit.formacio;
+
+/**
+ * Hello world!
+ *@author avidal
+ */
+public class App {
+  
+    public static String getMessage () {
+        return "Hello World!";
+    }
+}
